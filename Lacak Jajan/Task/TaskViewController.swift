5f06666c0bfc1d20bd52244edd9b63f31b84b36c
Tommy Miyazaki <tommyzki@Toms-MacBook-Pro.local>
2017-07-18 11:41:15 +0700

    Task: Change RefreshPage Function

--- Lacak Jajan/Task/TaskViewController.swift
+++ Lacak Jajan/Task/TaskViewController.swift
@@ -12,61 +12,32 @@ import Money
 
 class TaskViewController: UIViewController, Table2Delegate, NabungDelegate, TarikDelegate, TransferDelegate, JajanDelegate {
     
-    let userzat = Users()
-    var userObject: UserObject = UserObject()
-
     @IBOutlet weak var saldoKasUser: UILabel!
     
     @IBOutlet weak var saldoTabunganUser: UILabel!
     
     func table2WillDismissed(){
         self.dismiss(animated: true, completion: nil)
-        viewDidAppear(false)
+        refreshpage()
     }
     
     func refreshpage() {
-        viewDidAppear(false)
+        
+        let kasTemp: Money = (Money(userObject.kas))
+        let kasRupiah = "\(kasTemp)"
+        
+        let tabunganTemp: Money = Money(userObject.tabungan)
+        let tabunganRupiah = "\(tabunganTemp)"
+        
+        self.saldoKasUser.text = "\((kasRupiah))"//.substring(from: kasRupiah.index(after: kasRupiah.startIndex )))"
+        self.saldoTabunganUser.text = "\((tabunganRupiah))"//.substring(from: tabunganRupiah.index(after: tabunganRupiah.startIndex )))"
+
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        refreshpage()
         view.addBackground()
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        
-        let kasTemp: Money = (Money(userObject.kas))
-        let kasRupiah = "\(kasTemp)"
-
-        let tabunganTemp: Money = Money(userObject.tabungan)
-        let tabunganRupiah = "\(tabunganTemp)"
-
-        self.saldoKasUser.text = "\((kasRupiah))"//.substring(from: kasRupiah.index(after: kasRupiah.startIndex )))"
-        self.saldoTabunganUser.text = "\((tabunganRupiah))"//.substring(from: tabunganRupiah.index(after: tabunganRupiah.startIndex )))"
-
-        
-        
-//        let username = UserDefaults.standard.object(forKey: "userzat") as! String
-//        
-//        userzat.loadUser(loginusername: username,completed: { (success) -> Void in
-//            SVProgressHUD.dismiss()
-//            
-//            if success { // this will be equal to whatever value is set in this method call
-//                
-//                
-//                let kasTemp: Money = (Money(self.userzat.kas))
-//                let kasRupiah = "\(kasTemp)"
-//                
-//                let tabunganTemp: Money = Money(self.userzat.tabungan)
-//                let tabunganRupiah = "\(tabunganTemp)"
-//                
-//                self.saldoKasUser.text = "\((kasRupiah))"//.substring(from: kasRupiah.index(after: kasRupiah.startIndex )))"
-//                self.saldoTabunganUser.text = "\((tabunganRupiah))"//.substring(from: tabunganRupiah.index(after: tabunganRupiah.startIndex )))"
-//            } else {
-//                self.showAlert("Gagal Load Data", title: "Error")
-//            }
-//        })
     }
 
     override func didReceiveMemoryWarning() {
@@ -100,23 +71,18 @@ class TaskViewController: UIViewController, Table2Delegate, NabungDelegate, Tari
         
         if segue.identifier == "JajanSegue" {
             let js = segue.destination as! JajanViewController
-            js.userzat = self.userzat
             js.jajanDelegate = self
         } else if segue.identifier == "TransferSegue" {
             let tf = segue.destination as! TransferViewController
-            tf.userzat = self.userzat
             tf.transferDelegate = self
         } else if segue.identifier == "TarikSegue" {
             let ts = segue.destination as! TarikDuitViewController
-            ts.userzat = self.userzat
             ts.tarikDelegate = self
         } else if segue.identifier == "NabungSegue" {
             let ns = segue.destination as! NabungViewController
-            ns.userzat = self.userzat
             ns.nabungDelegate = self
         } else if segue.identifier == "MasukSegue" {
             let dd = segue.destination as! DapetDuitViewController
-            dd.userzat = self.userzat
             dd.table2Delegate = self
         }
         
